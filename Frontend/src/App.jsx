a6672e0ca6cae0062517282492029d1fe3c8f573
Manish Jatav <manishjatav041@gmail.com>
2026-07-08 17:38:16 +0530

    v2: current working version

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,23 @@
-import { Routes, Route } from "react-router-dom";
-
-import Navbar from "./components/Navbar/Navbar";
-import Home from "./Pages/Home";
-import Login from "./Pages/Login";
-import Register from "./Pages/Register";
-import Dashbaord from "./Pages/Dashbaord";
-import TodayAttendance from "./Pages/TodayAttendace";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
+  const token = localStorage.getItem("token");
+
   return (
     <div className="min-h-screen bg-gradient-to-b from-[#f2f0ff] to-white font-normal tracking-tight">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={token ? <Navigate to="/dashboard" replace /> : <Home />}
+        />
+
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashbaord />} />
-        <Route path="/dashboard/today-attendance" element={<TodayAttendance />} />
+        <Route
+          path="/dashboard/today-attendance"
+          element={<TodayAttendance />}
+        />
       </Routes>
     </div>
   );
