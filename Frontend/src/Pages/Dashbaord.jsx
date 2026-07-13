58554aa263bb718ef450d1af9ff696b0d4443fcb
Manish Jatav <106468581+Manishjatav@users.noreply.github.com>
2026-07-13 15:59:03 +0530

    Update Dashbaord.jsx

--- Frontend/src/Pages/Dashbaord.jsx
+++ Frontend/src/Pages/Dashbaord.jsx
@@ -45,18 +45,13 @@ const Dashbaord = () => {
 
               {/* CGPA Card */}
               <div className="flex items-center justify-between rounded-2xl border border-slate-200 bg-white p-6 shadow-sm">
-                <div className="flex flex-col">
-                  <h3 className="text-sm font-medium text-slate-800">
-                    Current CGPA
-                  </h3>
+                <Button className="text-sm font-medium bg-green-500" onClick={() => navigate('/dashboard/cgpa')}>
+                    Track CGPA<MdArrowOutward size={30} />
+                  </Button>
 
-                  <h1 className="mt-1 text-2xl font-bold text-blue-600">
-                    NaN
-                  </h1>
-
-                  <p className="mt-1 text-xs text-slate-500">
-                    On a scale of 10
-                  </p>
+                   <Button className="text-sm font-medium bg-blue-500" onClick={() => navigate('/dashboard/today-attendance')}>
+                    Today Attendance<MdArrowOutward size={30} />
+                  </Button>
                 </div>
 
                 <div className="flex h-14 w-14 items-center justify-center rounded-full bg-indigo-50">
